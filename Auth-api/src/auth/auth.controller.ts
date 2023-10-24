a76d1f1c7650658ff859e2c8c18f9547be94d94b
Randrin Nzeukang <nrandrin@eservicios.indracompany.com>
2023-10-24 05:55:20 +0200

    Authentication commit

---
+++ Auth-api/src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/sign-up.dto';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signUp(@Body() singUpDto: SignUpDto): Promise<{ token: string }> {
+    return this.authService.signUp(singUpDto);
+  }
+
+  @Get('/login')
+  login(@Body() loginDto: LoginDto): Promise<{ token: string }> {
+    return this.authService.login(loginDto);
+  }
+}
